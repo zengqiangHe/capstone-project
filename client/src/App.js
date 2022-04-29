739e0763f7ba33087913559d9d9c88df0d5e914a
zengqiangHe <zengqiang.he@outlook.com>
2022-04-29 09:58:48 +0200

    add cards with initial data

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,46 @@
-import { useState } from 'react';
 import './App.css';
+import EventCard from './components/eventCard/EventCard';
+import Header from './components/header/Header';
+
 
 function App() {
-  const [users, setUsers] = useState([]);
-
-  function fetchSomething() {
-    fetch('/api')
-      .then(res => res.json())
-      .then(data => setUsers(data));
-  }
+  const eventsList = [
+    {
+      id: '1',
+      title: 'Dumplings Party',
+      text: 'Wollen wir mal wieder zusammen Dumplings bei mir Zuhause machen?',
+      date_time: '13-06-2022 19 Uhr',
+      location: 'Harvestehude, Hamburg',
+    },
+    {
+      id: '2',
+      title: 'Restaurant Besuch bei Akari',
+      text: 'Habt Ihr Bock auf leckeres japanisches Essen?',
+      date_time: '08-07-2022 19 Uhr',
+      location: 'Papenhuder Str. 67, Hamburg',
+    },
+    {
+      id: '3',
+      title: 'After Work Run an der Alster',
+      text: 'Am Dienstag haben wir endlich gutes Wetter in Hamburg! Wollen wir nach der Arbeit zusammen Joggen gehen?',
+      date_time: '11-08-2022 18 Uhr',
+      location: 'An der Alster, Hamburg',
+    },
+  ];
 
   return (
     <div className="App">
-      <h1>Users</h1>
-      <button onClick={fetchSomething}>Fetch users</button>
-      <ul>
-        {users.map(user => (
-          <li key={user.id}>{user.name}</li>
+      <Header />
+      <ul style={{padding: '5%'}} role="list">
+        {eventsList.map(event => (
+          <EventCard
+            key={event.id}
+            id={event.id}
+            title={event.title}
+            text={event.text}
+            date_time={event.date_time}
+            location={event.location}
+          />
         ))}
       </ul>
     </div>
---
+++ client/src/pages/GlobalStyles.jsx
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle `
+* {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  :root {
+    --white: #ffffff;
+    --blue: #00A3FF;
+    --orange: #FD9F2E;
+    --darkred: #FF666;
+    --darkpink: #F06565;
+    --darkgrey: #E5E5E5;
+    --black: #181818;
+
+
+  }
+
+  body {
+    min-height: 100vh;
+    background-color: var(--darkgrey);
+    font-family: 'Libre Baskerville', serif, Helvetica, sans-serif;
+    font-size: 1rem;
+    line-height: 1.2;
+    color: var(--white);
+  }
+`
