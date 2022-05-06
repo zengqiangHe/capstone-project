6190fa8645b5de20d4f7373ffd25e404cf789069
zengqiangHe <zengqiang.he@outlook.com>
2022-05-06 12:23:00 +0200

    install cors

--- server/server.js
+++ server/server.js
@@ -3,19 +3,8 @@ import path from 'path';
 import api from './routes/api.js';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import cors from 'cors';
 
-const express = require('express')
-const cors = require('cors')
-
-app.use(cors())
-
-app.get('/products/:id', function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for all origins!'})
-})
-
-app.listen(80, function () {
-  console.log('CORS-enabled web server listening on port 80')
-})
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -24,6 +13,8 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
+
+app.use(cors());
 
 // Serve the React static files after build
 app.use(express.static(path.resolve(__dirname, '../client/build')));
